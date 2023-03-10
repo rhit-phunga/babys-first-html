c48b0a61a888939a3c30eb01387e5bebff4ac412
Khai Phung <phunga@rose-hulman.edu>
2023-03-10 15:20:52 -0500

    made some changes

---
+++ excersizes/button.js
@@ -0,0 +1,6 @@
+const btn3 = document.getElementById("button3")
+btn3.addEventListener("click", function() {
+alert("Hello Mars!");
+})
+
+
--- test.js
+++
@@ -1,1 +0,0 @@
-console.log("Hey, you. You're finally awake. You were trying to cross the border, right? Walked right into that Imperial ambush, same as us, and that thief over there.");
